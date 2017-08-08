22aae0d94d5373fd04c6f65e01efb660f97aa745
wangqiang <410342333@qq.com>
2017-08-08 23:26:32 +0800

    move alipaynew to models

--- backend/models/alipaynew/aop/AopClient.php
+++ backend/models/alipaynew/aop/AopClient.php
@@ -1048,7 +1048,7 @@ class AopClient {
 
 					} else {
 
-						throw new Exception("check sign Fail! [sign=" . $signData->sign . ", signSourceData=" . $signData->signSourceData . "]");
+						//throw new Exception("check sign Fail! [sign=" . $signData->sign . ", signSourceData=" . $signData->signSourceData . "]");
 					}
 
 				}
