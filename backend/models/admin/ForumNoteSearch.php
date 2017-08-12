4e33b74f7a4196ba41570ff3581de4703ba37b0a
wangqiang <410342333@qq.com>
2017-08-12 23:16:44 +0800

    sort forum notes desc

--- backend/models/admin/ForumNoteSearch.php
+++ backend/models/admin/ForumNoteSearch.php
@@ -47,6 +47,11 @@ class ForumNoteSearch extends ForumNote
 
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
+            'sort'=>[
+                'defaultOrder' => [
+                    'id' => SORT_DESC,
+                ]
+            ],
         ]);
 
         $this->load($params);
