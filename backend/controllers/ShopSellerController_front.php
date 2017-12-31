89331f0247c0d8f925cb268b463a2a4cfc69ad07
yeicandoit <410342333@qq.com>
2017-12-31 22:13:01 +0800

    Add mobile page for test center shop

--- backend/controllers/ShopSellerController_front.php
+++ backend/controllers/ShopSellerController_front.php
@@ -65,6 +65,7 @@ class ShopSellerController extends Controller
     		$is_mobile = false;
     	}
     	 
+    		$is_mobile = false;
     	
     	$relatedExperts = $expert->relatedExperts;
     	 
@@ -86,10 +87,51 @@ class ShopSellerController extends Controller
     {
         $lab = Seller::findOne($id);
         $labInfo = $lab->ext;
-        $pages = new Pagination(['totalCount' =>$lab->pageGoods()->count(), 'pageSize' => '12']);
-        $model = $lab->pageGoods()->offset($pages->offset)->limit($pages->limit)->all();
+        $pages = new Pagination(['totalCount' =>$lab->pageGoods()->count(), 'pageSize' => 12]);
+        $model = $lab->pageGoods()->orderBy('id DESC')->offset($pages->offset)->limit($pages->limit)->all();
         $relatedLabs = $lab->relatedLabs;
-        return $this->render('lab', ['lab'=>$lab, 'labInfo'=>$labInfo, 'model'=>$model, 'pages' => $pages, 'relatedLabs'=>$relatedLabs]);
+
+        if ($this->isMobile()){
+            $model = $lab->pageGoods()->orderBy('id DESC')->offset(0)->limit(3)->all();
+            return $this->renderPartial('labmobile', ['lab' => $lab, 'labInfo' => $labInfo, 'model' => $model, 'relatedLabs' => $relatedLabs]);
+        } else {
+            return $this->render('lab', ['lab' => $lab, 'labInfo' => $labInfo, 'model' => $model, 'pages' => $pages, 'relatedLabs' => $relatedLabs]);
+        }    
+    }
+
+    public function actionShopgoods()
+    {
+        if (Yii::$app->request->isAjax) 
+        {
+            $pagenunber=(int)Yii::$app->request->get('page');
+            $shopid=(int)Yii::$app->request->get('shopid');
+            if(is_numeric($pagenunber) && is_numeric($shopid))
+            {
+                $offset = $pagenunber * 3;
+                $goodall = Seller::findOne($shopid)->pageGoods()->orderBy('id DESC')->offset($offset)->limit(3)->all();
+                //将商品信息封装
+                $onegoodinfo=array();
+                $allgoodinfo=array();
+                foreach ($goodall as $mm)
+                {
+                    $onegoodinfo['id']=$mm->id;
+                    $onegoodinfo['img']=$mm->img;
+                    $onegoodinfo['name']=$mm->name;
+                    $onegoodinfo['brand']=$mm->brand->name;
+                    $onegoodinfo['brandversion']=$mm->brandversion;
+                    $onegoodinfo['sell_price']=$mm->sell_price . "元/样";
+                    if(0 == $mm->sell_price){
+                        $onegoodinfo['sell_price']="价格面议"; 
+                    }
+                    $onegoodinfo['market_price']=$mm->market_price . "元/样";
+                    if(0 == $mm->market_price){
+                        $onegoodinfo['market_price']="价格面议"; 
+                    }
+                    array_push($allgoodinfo, $onegoodinfo);
+                }
+                return json_encode($allgoodinfo);
+            }
+        }
     }
 
     public function actionFavoriteexpert($id, $type)
@@ -127,5 +169,28 @@ class ShopSellerController extends Controller
         $model = $simulate->pageGoods()->offset($pages->offset)->limit($pages->limit)->all();
         $relatedLabs = $simulate->relatedLabs;
         return $this->render('simulate', ['lab'=>$simulate, 'labInfo'=>$simulateInfo, 'model'=>$model, 'pages' => $pages, 'relatedLabs'=>$relatedLabs]);
+    }
+
+    private function isMobile()
+    {
+        $is_mobile = null;
+        if (isset($is_mobile)) {
+            echo $is_mobile;
+        }
+        if (empty($_SERVER['HTTP_USER_AGENT'])) {
+            $is_mobile = false;
+        } elseif (strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false // many mobile devices (all iPhone, iPad, etc.)
+            || strpos($_SERVER['HTTP_USER_AGENT'], 'Android') !== false
+            || strpos($_SERVER['HTTP_USER_AGENT'], 'Silk/') !== false
+            || strpos($_SERVER['HTTP_USER_AGENT'], 'Kindle') !== false
+            || strpos($_SERVER['HTTP_USER_AGENT'], 'BlackBerry') !== false
+            || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mini') !== false
+            || strpos($_SERVER['HTTP_USER_AGENT'], 'Opera Mobi') !== false) {
+                $is_mobile = true;
+            } else {
+                $is_mobile = false;
+            }
+
+        return $is_mobile;
     }
 }
