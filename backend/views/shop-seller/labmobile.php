550fa812a27a54f10539f429e323b4ebc63572a3
yeicandoit <410342333@qq.com>
2018-01-14 17:15:11 +0800

    update shop lab mobile page

--- backend/views/shop-seller/labmobile.php
+++ backend/views/shop-seller/labmobile.php
@@ -10,8 +10,10 @@ use yii\helpers\Url;
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="./jquery.min.js"></script>
     <script src="./jqmb/jquery.mobile-1.4.5.min.js"></script>
+    <script src="./raty/lib/jquery.raty.js"></script>
     <link rel="stylesheet" href="./jqmb/jquery.mobile-1.4.5.min.css">
     <link rel="stylesheet" href="./css/sellerhome.css">
+    <link rel="stylesheet" href="./raty/lib/jquery.raty.css">
     <title>检测中心实验室</title>
     <style type="text/css">
         Table tbody tr {
@@ -68,16 +70,25 @@ use yii\helpers\Url;
         .ui-table-columntoggle-btn {
             display: none !important;
         }
+        a {
+            color:#3399FF;
+            font-weight:Normal; /*CSS字体效果 普通 可以改成bold粗体 如果去除此行那么默认是不显示下划线的*/
+            text-decoration:none; /*CSS下划线效果：无下划线*/
+        }
     </style>
     <script  type="text/javascript">
         function showlab(id)
         {
-            for(var i=0; i<4; i++){
+            for(var i=1; i<=4; i++){
                 $("#showlab_"+i).hide();
             }
             $("#showlab_"+id).show();
         }
 
+	    $(document).on("tap", ".myp2", function(){
+	        var tmp=$(this).siblings('input').val();
+	        window.location.href="<?=Url::to(['site/goodinfo'])?>&id="+tmp;
+	    });
         var pagenum=0;
         $(document).on("pagecreate","#pageone",function(){
             $("#addmore").on("tap",function(){
@@ -89,19 +100,23 @@ use yii\helpers\Url;
                         var myobj = eval('(' + result + ')');
                         for(var i=0;i<myobj.length;i++){  
                             var str1="<tr>";
-                            var str2= "<td style='width:8rem;'>  <img src= "    + myobj[i]['img'] + " alt='Norway' style='width:100%'>    </td>";
-                            var str3="<td>  <div class='myp'>" +  myobj[i]['name']  + "</div>";
-                            var str4= "<div class='myp'>品  &nbsp&nbsp 牌："+  myobj[i]['brand']+ "</div>";
-                            var str5= "<div class='myp'>型  &nbsp&nbsp 号："+  myobj[i]['brandversion']+ "</div>";
-                            var str6="<div class='myp'>销售价："+  myobj[i]['sell_price']+ "</div>";
-                            var str7="	 <div class='myp' style='text-decoration:line-through;color:red'>市场价："+  myobj[i]['market_price']+ "</div>";
-                            var str9="</tr>  ";
+                            var str2= "<td style='width:8rem;'>  <img src= "    + myobj[i]['img'] + " alt='Norway' style='width:100%' class='myp2'>    </td>";
+                            var str3="<td>  <div class='myp2'>" +  myobj[i]['name']  + "</div>";
+                            var str4= "<div class='myp2'>品  &nbsp&nbsp 牌："+  myobj[i]['brand']+ "</div>";
+                            var str5= "<div class='myp2'>型  &nbsp&nbsp 号："+  myobj[i]['brandversion']+ "</div>";
+                            var str6="<div class='myp2'>销售价："+  myobj[i]['sell_price']+ "</div>";
+                            var str7="	 <div class='myp2' style='text-decoration:line-through;color:red'>市场价："+  myobj[i]['market_price']+ "</div>";
+                            var str9="</td></tr>  ";
 
                             var str=str1+str2+str3+str4+str5+str6+str7+str9;
                             $("#goodTable").append(str);
                         }  
                     }});
             })
+            $(".myp").on("tap",function(){
+		        var tmp=$(this).siblings('input').val();
+	            window.location.href="<?=Url::to(['site/goodinfo'])?>&id="+tmp;
+	        });
         });
     </script>
 </head>
@@ -115,7 +130,7 @@ use yii\helpers\Url;
              <a href="<?=Url::to(['site/login'])?>" data-ajax="false" class="ui-btn ui-corner-all ui-icon-user ui-btn-icon-notext"></a>	
         <?php endif;?>
         <?php  if (!Yii::$app->user->isGuest):?>
-             <a href="#page-logined"  class="ui-btn ui-corner-all ui-icon-user ui-btn-icon-notext" style="background-color:blue"></a>	
+             <a href="#page-logined" class="ui-btn ui-corner-all ui-btn-icon-right"><?= Yii::$app->user->identity->username ?></a>	
         <?php endif;?>
 
         <div data-role="navbar">
@@ -126,6 +141,7 @@ use yii\helpers\Url;
                 <li><a href="#" onclick="showlab(4)">科研成果</a></li>
             </ul>
         </div>
+        <br> <!-- To make navbar to be contained in header, user br. To find why or user more reasonable way -->
     </div>   <!-- pageone header -->
 
     <div data-role="main" class="ui-content">
@@ -146,7 +162,7 @@ use yii\helpers\Url;
                 <?php foreach ($model as $k=>$g): ?>
                     <tr>
                         <td style="width:8rem;">
-                            <img src="<?= $g['img'] ?>" alt="Norway" style="width:100%">
+                            <img src="<?= $g['img'] ?>" alt="Norway" style="width:100%" class="myp">
                         </td>
                         <td>
                             <div class="myp"><?= Html::encode($g['name']); ?></div>
@@ -192,7 +208,13 @@ use yii\helpers\Url;
         <div class="ui-grid-a ui-responsive">
             <div class="ui-block-a">
                 <div style="width:50%;margin:auto">
-                    <?= Html::img($lab->logo)?>
+                    <?php 
+                        $logo = $lab->logo;
+                        if("images/" == $logo || "" == $logo){
+                            $logo = "img/1111.jpg";
+                        }
+                    ?>
+                    <?= Html::img($logo,['style' => 'width:200px;height:200px;padding-top:5px'])?>
                 </div>
             </div>
             <div class="ui-block-b">
@@ -201,8 +223,13 @@ use yii\helpers\Url;
                     <tbody>
                         <tr><td>名称</td><td><?= $lab->true_name?></td></tr>
                         <tr><td>所在地</td><td><?= $lab->getLocation("/")?></td></tr>
-                        <?php $grade = 14 * $lab->grade?>
-                        <tr><td>评分</td><td><span class="grade"><i style=<?="width:$grade" . "px"?>></i></span></td></tr>
+                        <tr><td>评分</td><td><div style="width:10rem;float:left;height:32px" id="grate">
+                            </div>
+                            <div style="width:50px;float:left;height:32px">
+                            <?= Html::encode($lab->grade);?>
+                            </div>
+                            <script>    $('#grate').raty({ score: <?= Html::encode($lab->grade);?>});   </script>
+                        </td></tr>
                         <?php if("" != $lab->ext->reserve1){?>
                             <tr><td>宣传语</td><td><?=$lab->ext->reserve1?></td></tr>
                         <?php }?>
@@ -219,12 +246,19 @@ use yii\helpers\Url;
                 <tbody>
                 <?php if(null != $relatedLabs){?>
                 <?php foreach ($relatedLabs as $k=>$v): ?>
+                    <?php 
+                        $logo = $v->logo;
+                        if("images/" == $logo || "" == $logo){
+                            $logo = "img/1111.jpg";
+                        }
+                    ?>
+
                     <tr>
                     <td>
-                        <a href=<?=Url::to(['shop-seller/lab', 'id'=>$v->id])?> data-ajax="false"><?= Html::img("@web/$v->logo", ['style' => 'width:80px;height:60px;padding-top:5px']) ?></a>
+                        <a href=<?=Url::to(['shop-seller/lab', 'id'=>$v->id])?> data-ajax="false"><?= Html::img("@web/$logo", ['style' => 'width:80px;height:60px;padding-top:5px']) ?></a>
                     </td>
                     <td>
-                        <a href=<?=Url::to(['shop-seller/lab', 'id'=>$v->id])?> data-ajax="false"><?=$v->true_name?></a>
+                        <a href=<?=Url::to(['shop-seller/lab', 'id'=>$v->id])?>data-ajax="false"><?=$v->true_name?></a>
                     </td>
                     </tr>
                 <?php endforeach; ?>
@@ -235,6 +269,27 @@ use yii\helpers\Url;
 
     </div><!-- main -->
 </div>  <!-- pagetwo -->
+
+<div data-role="page" id="page-logined">
+    <div data-role="header">
+    <a href="#pageone" class="ui-btn ui-corner-all ui-icon-carat-l ui-btn-icon-notext">图标</a>
+      <h1></h1>
+    </div>
+
+    <div data-role="main" class="ui-content">
+    
+     <?php   if (!Yii::$app->user->isGuest): ?>
+  
+	<p style='font-size:1.5rem;'> 欢迎您,用户：<?= Yii::$app->user->identity->username ?></p><br>
+   	 <form id="form1" method="post" data-ajax="false" action="<?=Url::to(["site/logout"])?>">
+  	 <input type="hidden"      name="<?= \Yii::$app->request->csrfParam; ?>"     value="<?= \Yii::$app->request->getCsrfToken();?>">
+        <input type="submit" data-inline="true" value="注销">
+      </form>
+    <?php endif;?>
+    </div>
+    <div data-role="footer">
+    </div>
+</div> <!-- page-logined -->
 
 </body>
 </html>
